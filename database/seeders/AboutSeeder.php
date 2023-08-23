b0e7cbd71982fed3ac439e4fa64b93d001e0e85a
abduraxmon <abduraxmon.jalolov12@gmail.com>
2023-08-23 20:25:51 +0500

    jhdasdya

--- database/seeders/AboutSeeder.php
+++ database/seeders/AboutSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\About;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class AboutSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        About::created([
+            'phone_number_1'=>'941070524',
+            'phone_number_2'=>'941070524',
+            'email'=>'dfada@gmail.com',
+            'instagram'=>'afafafaef',
+            'facebook'=>'afafafaef',
+            'telegram'=>'afafafaef',
+            'description'=>'afafafaef SHDajsydasdu asuiawhd iawuhd',
+            'image'=>'logo.png',
+        ]);
     }
 }

c9ac009f4b432a7496011191d04543c79001549d
Yahyobek <mywhite060608@gmail.com>
2023-08-24 21:53:43 +0500

    tour page

--- app/Http/Livewire/Frontend/Tours.php
+++ app/Http/Livewire/Frontend/Tours.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\Frontend;
 
 use Livewire\Component;
+use App\Models\Tour;
 
 class Tours extends Component
 {
     public function render()
     {
-        return view('livewire.frontend.tours');
+
+        $tours = Tour::take(12)->orderByDesc('id')->get();;
+        return view('livewire.frontend.tours', compact('tours'));
     }
 }

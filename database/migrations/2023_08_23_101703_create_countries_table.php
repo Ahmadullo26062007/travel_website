4edebccca3b218bba859ff8dd43b87dd04413ed0
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-08-30 21:31:22 +0500

    add type to countries table

--- database/migrations/2023_08_23_101703_create_countries_table.php
+++ database/migrations/2023_08_23_101703_create_countries_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('image');
             $table->string('flag');
+            $table->enum('type',[0,1])->default(0);
 
             $table->timestamps();
         });
